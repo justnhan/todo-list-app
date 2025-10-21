9bea91b2bb316b7dfadc6b244f72b650a85a6fd4
justnhan <nhandz1252006@gmail.com>
2025-10-21 14:13:47 +0700

    feat:Implement function to change status of a task

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 ﻿tasks = []
 def add_task(task_name):
     """Thêm một công việc mới vào danh sách"""
-    tasks.append(task_name)
+    dictionary_tasks = {'name': 'Tên công việc', 'completed': False}
+    dictionary_tasks ['name'] = task_name
+    tasks.append(dictionary_tasks)
     print(f"Đã thêm công việc:'{task_name}'")
 def list_tasks():
     for i in range (len(tasks)):
-        print (tasks[i])
+        if tasks[i]['completed']==True:
+            print("[X]", tasks[i]['name'])
+        else:
+            print("[ ]",tasks[i]['name'])
+def complete_task(task_index):
+    tasks [task_index] ['completed']=True
 #---Điểm bắt đầu của chương trình---
 if __name__ == "__main__":
     print("Chào mừng đến với ứng dụng To-do-list!")
     add_task("Học bài Git và Github")
     add_task("Làm bài tập thực hành ở nhà")
+    complete_task(0)
     list_tasks()
